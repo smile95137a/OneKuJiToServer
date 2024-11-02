fce29d0d3d54b97357bcd87b13e53aabe6e0d765
Jimmy <a0970124936@gmail.com>
2024-11-02 18:37:12 +0800

    update

--- frontend/src/main/java/com/one/frontend/service/PaymentService.java
+++ frontend/src/main/java/com/one/frontend/service/PaymentService.java
@@ -63,8 +63,8 @@ public class PaymentService {
 
     public PaymentResponse creditCard(PaymentRequest paymentRequest) {
 
-        String url = "https://n.gomypay.asia/ShuntClass.aspx";  //正式
-//        String url = "https://n.gomypay.asia/TestShuntClass.aspx";  //測試
+//        String url = "https://n.gomypay.asia/ShuntClass.aspx";  //正式
+        String url = "https://n.gomypay.asia/TestShuntClass.aspx";  //測試
 
         PaymentRequest req = PaymentRequest.builder()
                 .sendType("0".trim())  // 傳送型態，寫死去除空白
@@ -137,8 +137,8 @@ return null;
 
 
     public PaymentResponse webATM(PaymentRequest paymentRequest) {
-        String url = "https://n.gomypay.asia/ShuntClass.aspx";  //正式
-//        String url = "https://n.gomypay.asia/TestShuntClass.aspx";  //測試
+//        String url = "https://n.gomypay.asia/ShuntClass.aspx";  //正式
+        String url = "https://n.gomypay.asia/TestShuntClass.aspx";  //測試
 
         PaymentRequest req = PaymentRequest.builder()
                 .sendType("4".trim())  // 傳送型態，去除空白
@@ -197,8 +197,8 @@ return null;
     }
 
     public PaymentResponse webATM2(PaymentRequest paymentRequest) {
-        String url = "https://n.gomypay.asia/ShuntClass.aspx";  //正式
-//        String url = "https://n.gomypay.asia/TestShuntClass.aspx";  //測試
+//        String url = "https://n.gomypay.asia/ShuntClass.aspx";  //正式
+        String url = "https://n.gomypay.asia/TestShuntClass.aspx";  //測試
 
         PaymentRequest req = PaymentRequest.builder()
                 .sendType("4".trim())  // 傳送型態，去除空白
