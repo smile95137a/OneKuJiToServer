bdb624cd3038778e82bde3353b86f4eab44a5521
Jimmy <a0970124936@gmail.com>
2024-11-18 18:23:32 +0800

    no message

--- frontend/src/main/java/com/one/frontend/controller/ExpressController.java
+++ frontend/src/main/java/com/one/frontend/controller/ExpressController.java
@@ -75,11 +75,11 @@ public class ExpressController {
         // 設定 POST 的參數
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         if("1".equals(codeRequest.getX())){ //1是mall 2是賞品盒S
-//            params.add("Url", "https://onemorelottery.tw/mall-checkout");
-            params.add("Url", "http://localhost:5173/mall-checkout");
+            params.add("Url", "https://onemorelottery.tw/mall-checkout");
+//            params.add("Url", "http://localhost:5173/mall-checkout");
         }else if ("2".equals(codeRequest.getX())){
-//            params.add("Url", "https://onemorelottery.tw/member-center/prize-checkout");
-            params.add("Url", "http://localhost:5173/member-center/prize-checkout");
+            params.add("Url", "https://onemorelottery.tw/member-center/prize-checkout");
+//            params.add("Url", "http://localhost:5173/member-center/prize-checkout");
         }
 
         params.add("Opmode", "711".equals(code) ? "3" : ("family".equals(code) ? "1" : "0"));
