9836dae318f7cf0463961e51033ea7517c32376d
jim <>
2024-12-12 11:00:35 +0800

    調整功能

--- frontend/src/main/java/com/one/frontend/config/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ frontend/src/main/java/com/one/frontend/config/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -2,6 +2,7 @@ package com.one.frontend.config.security.oauth2;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -14,15 +15,18 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
+	@Value("${app.oauth.redirectUrl}")
+	private String redirectUrl;
 
-    @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-    	 exception.printStackTrace();
-    	 
-         String targetUrl = UriComponentsBuilder.fromUriString("/")
-                 .queryParam("error", exception.getLocalizedMessage())
-                 .build().toUriString();
-         
-         getRedirectStrategy().sendRedirect(request, response, targetUrl);
-    }
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		exception.printStackTrace();
+
+		String targetUrl = UriComponentsBuilder.fromUriString(redirectUrl)
+				.queryParam("errorMsg", String.format("oauth登錄失敗，原因: %s", exception.getMessage())).build()
+				.toUriString();
+		getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+	}
 }
