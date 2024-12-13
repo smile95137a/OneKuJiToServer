42e5358900963b4183e88df1ba75bececd4f4101
Jimmy <a0970124936@gmail.com>
2024-12-13 15:03:57 +0800

    保存本地更改到 OAuth2AuthenticationFailureHandler

--- frontend/src/main/java/com/one/frontend/config/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ frontend/src/main/java/com/one/frontend/config/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -1,15 +1,14 @@
 package com.one.frontend.config.security.oauth2;
 
-import java.io.IOException;
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
