e5bc4713b388b4e3c0800baf67926c145f8347ee
Jimmy <a0970124936@gmail.com>
2025-01-21 15:59:25 +0800

    no message

--- frontend/src/main/java/com/one/frontend/repository/CvsStoreInfoRepository.java
+++ frontend/src/main/java/com/one/frontend/repository/CvsStoreInfoRepository.java
@@ -2,9 +2,11 @@ package com.one.frontend.repository;
 
 import com.one.frontend.model.CvsStoreInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CvsStoreInfoRepository extends JpaRepository<CvsStoreInfo, Long> {
     Optional<CvsStoreInfo> findByUuid(String uuid);
 }
