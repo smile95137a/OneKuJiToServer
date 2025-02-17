f992a373999a995698588ccfeed086298d366feb
jim <>
2025-02-17 16:28:49 +0800

    調整功能

--- backend/src/main/java/com/one/onekuji/util/ImageUtil.java
+++ backend/src/main/java/com/one/onekuji/util/ImageUtil.java
@@ -142,8 +142,8 @@ public class ImageUtil {
         int originalHeight = originalImage.getHeight();
 
         // 定义目标尺寸
-        final int TARGET_WIDTH = 400;
-        final int TARGET_HEIGHT = 250;
+        final int TARGET_WIDTH = 1600;
+        final int TARGET_HEIGHT = 910;
 
         // 计算缩放比例
         double scale = Math.min(
